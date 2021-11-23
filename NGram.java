793153563fb0534025f009c814fd9cc338c81770
Matthew Neil <62803958+Matt-Neil@users.noreply.github.com>
2021-11-23 20:11:07 +0000

    Add files via upload

---
+++ NGram.java
@@ -0,0 +1,64 @@
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+
+public class BiGram {
+    public static class BGMapper extends Mapper <Object, Text, Text, IntWritable> {
+        private final static IntWritable one = new IntWritable(1);
+        private Text bigram = new Text();
+        List words = new ArrayList();
+
+        public void map(Object key, Text value, Context context) throws IOException, InterruptedException {
+            String sentence = value.toString().replaceAll("\\p{P}", "");
+            StringTokenizer itr = new StringTokenizer(sentence);  
+            
+            while (itr.hasMoreTokens()) {
+                words.add(itr.nextToken());
+            }
+
+            for (int i = 0; i < words.length; i++) {
+                if (i < words.length-1) {
+                    bigram.set(words[i] + " " + words[i+1]);
+                    context.write(bigram, one);
+                }
+            }
+        }
+    }
+
+    public static class BGReducer extends Reducer <Text, IntWritable, Text, IntWritable> {
+        private IntWritable result = new IntWritable();
+        
+        public void reduce (Text key, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException {
+            int sum = 0;
+
+            for (IntWritable val : values) {
+                sum += val.get();
+            }
+
+            result.set(sum);
+            context.write(key, result);
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        Configuration conf = new Configuration();
+        Job job = Job.getInstance(conf, "bigrams");
+        job.setJarByClass(BGram.class);
+        job.setMapperClass(BGMapper.class);
+        job.setReducerClass(BGReducer.class);
+        job.setOuputKeyClass(Text.class);
+        job.setInputValueClass(IntWritable.class);
+        FileInputFormat.addInputPath(job, new Path(args[0]));
+        FileOutputFormat.addOutputPath(job, new Path(args[1]));
+        System.exit(job.waitForCompletion(true) ? 0 : 1);
+    }
+}
