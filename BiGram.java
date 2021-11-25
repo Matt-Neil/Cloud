6e495ee3c098d8d531bf1d97c1532ded4f56cc46
Matthew Neil <62803958+Matt-Neil@users.noreply.github.com>
2021-11-25 15:14:06 +0000

    Update BiGram.java

--- BiGram.java
+++ BiGram.java
@@ -50,7 +50,7 @@ public class BiGram {
     }
 
     public class BGPartitioner extends Partitioner<Text,Text> {
-        public int getPartition(Text key, IntWritable value, int numReduceTasks) {
+        public int getPartition(Text key, Text value, int numReduceTasks) {
             String partitionKey = key.toString();
 
             if (numReduceTasks == 7) {
