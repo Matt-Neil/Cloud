5bd358f674093810515b7664d0696981dfc02901
Matthew Neil <62803958+Matt-Neil@users.noreply.github.com>
2021-11-25 17:24:26 +0000

    Update BiGram.java

--- BiGram.java
+++ BiGram.java
@@ -5,8 +5,7 @@ import java.io.IOException;
 import java.util.StringTokenizer;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.regex;
-import java.util.regex.Pattern;
+import java.util.regex.*;
 
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
