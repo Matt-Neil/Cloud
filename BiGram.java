421561f164dbe63e876e3a6d4de1cc123d521bf1
Matthew Neil <62803958+Matt-Neil@users.noreply.github.com>
2021-11-25 14:51:50 +0000

    Update BiGram.java

--- BiGram.java
+++ BiGram.java
@@ -15,6 +15,7 @@ import org.apache.hadoop.mapreduce.Mapper;
 import org.apache.hadoop.mapreduce.Reducer;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.mapreduce.Partitioner;
 
 public class BiGram {
 
@@ -48,37 +49,27 @@ public class BiGram {
         }
     }
 
-    // public class TOPartitioner {
-    //     public static void main(String[] args) throws Exception {
-     
-    //         // Create job and parse CLI parameters
-    //         Job job = Job.getInstance(new Configuration(), "Total Order Sorting example");
-    //         job.setJarByClass(TotalOrderPartitionerExample.class);
-            
-    //         Path inputPath = new Path(args[0]);
-    //         Path partitionOutputPath = new Path(args[1]);
-    //         Path outputPath = new Path(args[2]);
-     
-    //         // The following instructions should be executed before writing the partition file
-    //         job.setNumReduceTasks(3);
-    //         FileInputFormat.setInputPaths(job, inputPath);
-    //         TotalOrderPartitioner.setPartitionFile(job.getConfiguration(), partitionOutputPath);
-    //         job.setInputFormatClass(KeyValueTextInputFormat.class);
-    //         job.setMapOutputKeyClass(Text.class);
-            
-    //         // Write partition file with random sampler
-    //         InputSampler.Sampler<Text, Text> sampler = new InputSampler.RandomSampler<>(0.01, 1000, 100);
-    //         InputSampler.writePartitionFile(job, sampler);
-     
-    //         // Use TotalOrderPartitioner and default identity mapper and reducer 
-    //         job.setPartitionerClass(TotalOrderPartitioner.class);
-    //         job.setMapperClass(Mapper.class);
-    //         job.setReducerClass(Reducer.class);
-     
-    //         FileOutputFormat.setOutputPath(job, outputPath);
-    //         System.exit(job.waitForCompletion(true) ? 0 : 1);
-    //     }
-    // }
+    public class TOPartitioner extends Partitioner<Text,IntWritable,Text,IntWritable> {
+        @Override
+        public int getPartition(CompositeKey key, DonationWritable value, int numPartitions) {
+    
+            if (key.state.compareTo("E") < 0) {
+                return 0;
+            } else if (key.state.compareTo("I") < 0) {
+                return 1;
+            } if (key.state.compareTo("M") < 0) {
+                return 2;
+            } else if (key.state.compareTo("Q") < 0) {
+                return 3;
+            } if (key.state.compareTo("U") < 0) {
+                return 4;
+            } else if (key.state.compareTo("X") < 0) {
+                return 5;
+            } else {
+                return 6;
+            }
+        }
+    }
   
     public static class BGReducer extends Reducer<Text,IntWritable,Text,IntWritable> {
         private IntWritable result = new IntWritable();
@@ -99,21 +90,15 @@ public class BiGram {
       Configuration conf = new Configuration();
       Job job = Job.getInstance(conf, "bigram");
       job.setJarByClass(BiGram.class);
-
-      job.setNumReduceTasks(7);
-      FileInputFormat.addInputPath(job, new Path(args[0]));
-      TotalOrderPartitioner.setPartitionFile(job.getConfiguration, new Path(args[1]));
-      job.setInputFormatClass(KeyValueTextInputFormat.class);
-      job.setMapOutputKeyClass(Text.class);
-
-      InputSampler.Sampler<Text, Text> sampler = new InputSampler.RandomSampler<>(0.01, 1000, 100);
-      InputSampler.writePartitionFile(job, sampler);
-
       job.setMapperClass(BGMapper.class);
       job.setReducerClass(BGReducer.class);
       job.setCombinerClass(BGReducer.class);
-
-      FileOutputFormat.setOutputPath(job, new Path(args[2]));
+      job.setPartitionerClass(TOPartitioner.class);
+      job.setNumReduceTasks(7);
+      job.setOutputKeyClass(Text.class);
+      job.setOutputValueClass(IntWritable.class);
+      FileInputFormat.addInputPath(job, new Path(args[0]));
+      FileOutputFormat.setOutputPath(job, new Path(args[1]));
       System.exit(job.waitForCompletion(true) ? 0 : 1);
     }
 }
