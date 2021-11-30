c7768e3da3aef2081ca98e5d238dccd92b8edee0
Matthew Neil <62803958+Matt-Neil@users.noreply.github.com>
2021-11-30 17:38:18 +0000

    Update BiGram.java

--- BiGram.java
+++ BiGram.java
@@ -11,22 +11,22 @@ import java.io.DataOutput;
 
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
-import org.apache.hadoop.io.IntWritable;
-import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.Mapper;
 import org.apache.hadoop.mapreduce.Reducer;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 import org.apache.hadoop.mapreduce.Partitioner;
-import org.apache.hadoop.io.Writable;
-import org.apache.hadoop.io.WritableComparable;
-import org.apache.hadoop.io.WritableComparator;
+import org.apache.hadoop.io.*;
 
 public class BiGram {
 
     public class BG implements WritableComparable<BG> {
-        private String key;
+        private Text key;
+
+        public BG(Text key) {
+            this.key = key;
+        }
     
         public void write(DataOutput out) throws IOException {
             out.writeString(key);
